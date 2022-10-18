18b214fe246d6c21a0eea4fafca1f3a4eb0a541e
doko37 <hunubmm@gmail.com>
2022-10-19 09:57:48 +1300

    re-adding model

--- dry_matter_detection.py
+++ dry_matter_detection.py
@@ -5,7 +5,7 @@ import cv2
 import numpy as np
 import json
 
-model = tensorflow.keras.models.load_model('/home/kauricone/TeamSheep/GrassDetection/code/retrained-model/')
+model = tensorflow.keras.models.load_model('./retrained-model')
 
 height = 2560
 width = 1440
@@ -16,18 +16,29 @@ year = time.strftime("%Y")
 month = time.strftime("%m")
 day = time.strftime("%d")
 
-os.chdir("/home/kauricone/TeamSheep/SheepData/Images/{}/{}/{}/".format(year, month, day))
+# os.chdir("/home/kauricone/TeamSheep/SheepData/Images/{}/{}/{}/".format(year, month, day))
+os.chdir("./newImages/10/2")
 
 images = []
 
 for f in os.listdir("."):
     print(f)
     if f.endswith(".jpg"):
+
+        # Read in the image
         img = cv2.imread(f)
+
+        # Crop the image
         cropped_img = img[int(height * 0.38) : int(height - (height * 0.05)), int(width * 0.25) : int(width - (width * 0.10))]
+
+        # Calculate the average RGB values
         avg_col_row = np.average(cropped_img, axis=0)
         avg_col = np.average(avg_col_row, axis=0)
+
+        # Normalize
         avg_col = avg_col/(255.0)
+
+        # Append the raw data to array
         images.append([avg_col[0], avg_col[1], avg_col[2]])
 
 
@@ -40,18 +51,18 @@ results = [float(avg_pred), float(max), float(min)]
 
 print(results)
 
-def write_json(data, filename="/home/kauricone/TeamSheep/ObjectDetection/results.json"):
-    with open(filename, 'r+') as file:
-        # read in json file to variable
-        file_data = json.loads(file.read())
+# def write_json(data, filename="/home/kauricone/TeamSheep/ObjectDetection/results.json"):
+#     with open(filename, 'r+') as file:
+#         # read in json file to variable
+#         file_data = json.loads(file.read())
 
-        # add new data to json file
-        file_data["results"][-1]["grass"] = data
+#         # add new data to json file
+#         file_data["results"][-1]["grass"] = data
 
-        # move write head
-        file.seek(0)
+#         # move write head
+#         file.seek(0)
 
-        # write data to file
-        json.dump(file_data, file, indent=4)
+#         # write data to file
+#         json.dump(file_data, file, indent=4)
 
-write_json(results)
+# write_json(results)
